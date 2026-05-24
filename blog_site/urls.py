bb0500a2820b44dccbbf09d0e368aaf117ba0b3b
Dmytro <elder.evil.dev@proton.me>
2026-05-24 03:26:08 +0300

    fix: correct Wagtail API router URL inclusion
    - Use explicit include() with namespace for WagtailAPIRouter
    - Separate DRF router for custom blog endpoint
    - Fix Django 4+ include() 3-tuple compatibility

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.api.v2.router import WagtailAPIRouter
@@ -12,17 +13,23 @@ from wagtail.images.api.v2.views import ImagesAPIViewSet
 from home.api import BlogPageAPIViewSet
 from search.views import search
 
-api_router = WagtailAPIRouter("wagtailapi")
-api_router.register_endpoint("pages", PagesAPIViewSet)
-api_router.register_endpoint("images", ImagesAPIViewSet)
-api_router.register_endpoint("blog", BlogPageAPIViewSet)
+wagtail_api_router = WagtailAPIRouter("wagtailapi")
+wagtail_api_router.register_endpoint("pages", PagesAPIViewSet)
+wagtail_api_router.register_endpoint("images", ImagesAPIViewSet)
+
+drf_router = DefaultRouter()
+drf_router.register("blog", BlogPageAPIViewSet, basename="blog")
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search, name="search"),
-    path("api/v2/", api_router.urls),
+    path(
+        "api/v2/",
+        include((wagtail_api_router.get_urlpatterns(), "wagtailapi"), namespace="wagtailapi"),
+    ),
+    path("api/", include(drf_router.urls)),
 ]
 
 if settings.DEBUG:
